6798571d30017fdb22844543ca387bd7ad4749cf
BLUETOXINE <133049188+bluetox@users.noreply.github.com>
2025-04-24 10:23:06 +0200

    Delete kyber directory

--- kyber/src/rng.rs
+++
@@ -1,9 +0,0 @@
-use rand_core::*;
-
-// Fills buffer x with len bytes, RNG must satisfy the 
-// RngCore trait and CryptoRng marker trait requirements
-pub fn randombytes<R>(x: &mut [u8], len: usize, rng: &mut R)
-  where R: RngCore + CryptoRng,
-{
-  rng.fill_bytes(&mut x[..len]);
-}
